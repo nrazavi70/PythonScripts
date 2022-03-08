adefd0ce8214142785098d7c2e783b0373fe634c
nrazavi70 <n.razavi70@gmail.com>
2022-03-08 11:28:13 +0330

    List project more than 100

--- GitlabCLIUIResources/GitlabSource.py
+++ GitlabCLIUIResources/GitlabSource.py
@@ -5,9 +5,14 @@ from termcolor import colored
 gitlab_url = input(colored("Enter your gitlab url: ",'yellow'))
 
 token = input(colored("Enter token with api_read and api permission: ",'yellow'))
-projects_url = gitlab_url+"/api/v4/projects"
-projects_page_response = requests.get(projects_url, headers={'PRIVATE-TOKEN':token})
-projects = json.loads(projects_page_response.text)
+projects_url = gitlab_url+"/api/v4/projects?per_page=100&page="
+projects_page_responses = ""
+for i in range(10):
+    projects_page_response = requests.get(projects_url+str(i), headers={'PRIVATE-TOKEN':token})
+    if projects_page_response.text != "[]":
+        projects_page_responses = projects_page_responses + projects_page_response.text
+projects_page_responses = projects_page_responses.replace('][{"id',',{"id')
+projects = json.loads(projects_page_responses)
 
 def find_project(selected_project):
     for i in range(len(projects)):
--- GitlabCLIUIResources/ListProjects.py
+++ GitlabCLIUIResources/ListProjects.py
@@ -2,13 +2,10 @@ from GitlabCLIUIResources.GitlabSource import *
 from tabulate import tabulate
 
 def list_projects():
-    proj_table = [['ID','Name','Access-Level'],['-','-','-']]
+    proj_table = [['ID','Name'],['-','-']]
     print(colored('\nAll of your projects: ','green'))
     for i in range(len(projects)):
-        if projects[i]['permissions']['project_access'] is None:
-            proj_table.append([str(i+1),projects[i]['name'],projects[i]['permissions']['group_access']['access_level']])
-        else:
-            proj_table.append([str(i+1),projects[i]['name'],projects[i]['permissions']['project_access']['access_level']])
-        #print(str(i+1)+". "+projects[i]['name'])
+        proj_table.append([str(i+1),projects[i]['name']])
+        
     print(tabulate(proj_table))
     return None
